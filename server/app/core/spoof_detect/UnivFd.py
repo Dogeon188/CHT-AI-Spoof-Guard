09ded229815a1e955ff044a1c07500ac70810093
Dogeon188 <dogcatdog188@gmail.com>
2024-10-19 14:53:05 +0800

    [Mod] better connect to api [Add] test page for spoof detect [Rem] test images

--- server/app/core/spoof_detect/UnivFd.py
+++ server/app/core/spoof_detect/UnivFd.py
@@ -1,25 +1,33 @@
-from .base import SpoofDetectModel, SpoofDetectResult 
 import torch
 from PIL import Image
-from UniversalFakeDetect.models.clip_models import CLIPModel
+from .UniversalFakeDetect.models.clip_models import CLIPModel
 import os
+from urllib.request import urlopen
+from io import BytesIO
+
+from .base import SpoofDetectModel, SpoofDetectResult
+from app.api.models import ImageModel
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 
 class Univfd(SpoofDetectModel):
     def __init__(self):
         self.model = CLIPModel("ViT-L/14")
         self.model = self.add_fc(self.model)
-    
+
     def add_fc(self, model):
-        fc_weight = torch.load("UniversalFakeDetect/pretrained_weights/fc_weights.pth", map_location="cpu", weights_only=True)
+        cur_dir = os.path.dirname(__file__)
+        weight_path = os.path.join(
+            cur_dir, "UniversalFakeDetect/pretrained_weights/fc_weights.pth")
+        fc_weight = torch.load(
+            weight_path, map_location="cpu", weights_only=True)
         model.fc.load_state_dict(fc_weight)
         model = model.eval()
         model = model.to(device)
         return model
-    
-    def inference(self, image_path):
-        image = Image.open(image_path)
+
+    def inference(self, image):
         tensor = self.model.preprocess(image)
         batch = torch.stack([tensor])
         batch = batch.to(device)
@@ -28,27 +36,27 @@ class Univfd(SpoofDetectModel):
             out = self.model(batch)
             out = out.sigmoid()
         out = out.cpu()
-        print(out)
 
         out = out[0][0]
         return out
-    
-    def reference(self):
-        image_path = 'UniversalFakeDetect/whichfaceisreal/0_real/10286.jpeg'
-        prob = self.inference(image_path)
-        result = 'fake' if prob > 0.5 else 'real'
-        print(f'result = {result}, confidence = {prob}')
-        return SpoofDetectResult(
-            result=result,
-            confidence=prob
-        )
-    
-if __name__ == '__main__':
-    model = Univfd()
-    model.reference()
+
+    def reference(self, image: ImageModel) -> SpoofDetectResult:
+        # with tempfile.NamedTemporaryFile() as temp:
+        #     temp.write(image)
+        #     temp.seek(0)
+        with urlopen(image) as temp:
+            data = temp.read()
+            image = Image.open(BytesIO(data))
+            prob = self.inference(image)
+            result = 'spoof' if prob > 0.5 else 'real'
+            print(f"result: {result}, prob: {prob}")
+            confidence = (prob - 0.5) * 2 if result == 'spoof' else (0.5 - prob) * 2
+            return SpoofDetectResult(
+                result=result,
+                confidence=confidence
+            )
 
 
-
-
-
-        
+# if __name__ == '__main__':
+#     model = Univfd()
+#     model.reference()
