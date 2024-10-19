fa48bab1c9ac3561bb84fd64b0afa0702565f819
Dogeon188 <dogcatdog188@gmail.com>
2024-10-19 14:55:04 +0800

    [Fix] imagemodel should not be bool [Fix] CORS

--- server/app/main.py
+++ server/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .api import router
 
@@ -10,5 +11,12 @@ def main():
         version="0.0.0",
     )
     app.include_router(router)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     return app
